991d0d41fa2be4812537bab14665796697627040
bob <rea952@gmail.com>
2019-02-27 19:37:34 +1100

    New version of serial mux supporting all SERIAL devices.

--- tracker/software/source/drivers/mcu/serialmux.c
+++ tracker/software/source/drivers/mcu/serialmux.c
@@ -4,7 +4,7 @@
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/
+ */
 
 /**
  * @file    serialmux.c
@@ -13,152 +13,228 @@
  * @addtogroup drivers
  * @{
  */
+
 #include "hal.h"
-#include "pktconf.h"
 #include "serialmux.h"
 
-/*===========================================================================*/
-/* Module local definitions.                                                 */
-/*===========================================================================*/
+#if (HAL_USE_SERIAL && STM32_SERIAL_USE_MUX) || defined(__DOXYGEN__)
 
-BSEMAPHORE_DECL(serial_mux, false);
-event_source_t *esp;
-event_listener_t el;
+#include "pktconf.h"
+
 
 /*===========================================================================*/
-/* Module exported functions.                                                */
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/** @brief USART1 serial driver identifier.*/
+#if STM32_SERIAL_USE_USART1 || defined(__DOXYGEN__)
+SerialMuxDriver SM1;
+#endif
+
+/** @brief USART2 serial driver identifier.*/
+#if STM32_SERIAL_USE_USART2 || defined(__DOXYGEN__)
+SerialMuxDriver SM2;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_USART3 || defined(__DOXYGEN__)
+SerialMuxDriver SM3;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_UART4 || defined(__DOXYGEN__)
+SerialMuxDriver SM4;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_UART5 || defined(__DOXYGEN__)
+SerialMuxDriver SM5;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_USART6 || defined(__DOXYGEN__)
+SerialMuxDriver SM6;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_UART7 || defined(__DOXYGEN__)
+SerialMuxDriver SM7;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_SERIAL_USE_UART8 || defined(__DOXYGEN__)
+SerialMuxDriver SM9;
+#endif
+
+/** @brief USART3 serial driver identifier.*/
+#if STM32_HAS_LPUART1 || defined(__DOXYGEN__)
+#if STM32_SERIAL_USE_LPUART1
+SerialMuxDriver LPSM1;
+#endif
+#endif
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
 /*===========================================================================*/
 
 /**
+ * @brief   Low level serial multiplexer driver initialization.
+ *
+ * @notapi
+ */
+void smd_lld_init(void) {
+
+#if STM32_SERIAL_USE_USART1
+  chBSemObjectInit(&SM1.msem, false);
+  SM1.sd = &SD1;
+  SM1.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_USART2
+  chBSemObjectInit(&SM2.msem, false);
+  SM2.sd = &SD2;
+  SM2.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_USART3
+  chBSemObjectInit(&SM3.msem, false);
+  SM3.sd = &SD3;
+  SM3.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_UART4
+  chBSemObjectInit(&SM4.msem, false);
+  SM4.sd = &SD4;
+  SM4.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_UART5
+  chBSemObjectInit(&SM5.msem, false);
+  SM5.sd = &SD5;
+  SM5.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_USART6
+  chBSemObjectInit(&SM6.msem, false);
+  SM6.sd = &SD6;
+  SM6.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_UART7
+  chBSemObjectInit(&SM7.msem, false);
+  SM7.sd = &SD7;
+  SM7.esp = NULL;
+#endif
+
+#if STM32_SERIAL_USE_UART8
+  chBSemObjectInit(&SM8.msem, false);
+  SM8.sd = &SD8;
+  SM8.esp = NULL;
+#endif
+
+#if STM32_HAS_LPUART1
+#if STM32_SERIAL_USE_LPUART1
+  chBSemObjectInit(&LPSM1.msem, false);
+  LPSM1.sd = &LPSD1;
+  LPSM1.esp = NULL;
+#endif
+#endif
+}
+
+/**
  * @brief Connect to multiplexed serial channel
- * @note  Only supports UART based serial channels
+ * @note  Only supports UART/USART based serial channels
  *
  */
-msg_t pktConnectMuxedSerial(const SerialDriver *serial,
-		                    const SerialConfig *cfg,
-							const smux_gpio_t *gpio,
-                            const sysinterval_t timeout) {
-#if defined(SERIAL_MUX_DEVICE)
-  if (serial != &SERIAL_MUX_DEVICE) {
-    return MSG_ERROR;
-  }
+msg_t pktOpenMuxedSerial(SerialMuxDriver *const smd,
+                         const SerialConfig *sdcfg,
+                         const SerialMuxConfig *mcfg,
+                         const sysinterval_t timeout) {
   /* Acquire the multiplexed serial channel. */
-  msg_t msg = chBSemWaitTimeout(&serial_mux, timeout);
+  msg_t msg = chBSemWaitTimeout(&smd->msem, timeout);
   if (msg != MSG_OK) {
-	  return msg;
+    return msg;
   }
   /* Configure the GPIO to map to the UART. */
-  if (gpio->rx.line != PAL_NOLINE) {
-	  palSetLineMode(gpio->rx.line, gpio->rx.mode);
+  if (mcfg->open.rx.line != PAL_NOLINE) {
+    palSetLineMode(mcfg->open.rx.line, mcfg->open.rx.mode);
   }
-  if (gpio->tx.line != PAL_NOLINE) {
-	  palSetLineMode(gpio->tx.line, gpio->tx.mode);
+  if (mcfg->open.tx.line != PAL_NOLINE) {
+    palSetLineMode(mcfg->open.tx.line, mcfg->open.tx.mode);
   }
-  sdStart(&SERIAL_MUX_DEVICE, cfg);
+  sdStart(smd->sd, sdcfg);
   return MSG_OK;
-#endif
-  (void)serial;
-  (void)cfg;
-  (void)gpio;
-  (void)timeout;
-  return MSG_ERROR;
 }
 
 /**
  * @brief Disconnect multiplexed serial channel
  * @note  Only supports UART based serial channels
  *
+ * @param[in] smd       pointer to serial multiplexer driver.
+ * @param[in] timeout   the number of ticks before the operation timeouts, the
+ *                      special values are handled as follow:
+ *                      - @a TIME_INFINITE is allowed but interpreted as a
+ *                        normal time specification.
+ *                      - @a TIME_IMMEDIATE this value does not wait for the
+ *                        serial transmission to end.
  */
-msg_t pktDisconnectMuxedSerial(const SerialDriver *serial,
-							   const smux_gpio_t *gpio,
-							   const sysinterval_t timeout) {
-#if defined(SERIAL_MUX_DEVICE)
-  if (serial != &SERIAL_MUX_DEVICE) {
-    return MSG_ERROR;
-  }
-  sdStop(&SERIAL_MUX_DEVICE);
-  /* Remove the GPIO map to the UART. */
-  if (gpio->rx.line != PAL_NOLINE) {
-	  palSetLineMode(gpio->rx.line, PAL_MODE_INPUT);
-  }
-  if (gpio->tx.line != PAL_NOLINE) {
-	  palSetLine(gpio->tx.line);
-	  palSetLineMode(gpio->tx.line, PAL_MODE_OUTPUT_PUSHPULL);
-  }
-  /* Release port lock. */
-  chBSemSignal(&serial_mux);
-  return MSG_OK;
-#endif
-  (void)serial;
-  (void)gpio;
-  (void)timeout;
-  return MSG_ERROR;
-}
-
-/**
- *
- */
-msg_t pktAcquireMuxedSerial(const SerialDriver *serial,
-                            const sysinterval_t timeout) {
-#if defined(SERIAL_MUX_DEVICE)
-  if (serial != &SERIAL_MUX_DEVICE) {
-    return MSG_OK;
-  }
-  return chBSemWaitTimeout(&serial_mux, timeout);
-#else
-  (void)serial;
-  (void)timeout;
-  return MSG_ERROR;
-#endif
-}
-
-/**
- *
- */
-void pktReleaseMuxedSerial(const SerialDriver *serial) {
-#if defined(SERIAL_MUX_DEVICE)
-  if (serial == &SERIAL_MUX_DEVICE) {
-    sdStop(&SERIAL_MUX_DEVICE);
-    chBSemSignal(&serial_mux);
-  }
-#endif
-  (void)serial;
-}
-
-
-/**
- *
- */
-msg_t pktReleaseMuxedSerialEOT(const SerialDriver *serial, const uint8_t *out,
-                              const size_t len, const sysinterval_t timeout) {
-#if defined(SERIAL_MUX_DEVICE)
+msg_t pktCloseMuxedSerial(SerialMuxDriver *const smd,
+                          const SerialMuxConfig *mcfg,
+                          const uint8_t *out,
+                          const size_t len,
+                          const sysinterval_t timeout) {
   msg_t msg = MSG_OK;
-  if (serial == &SERIAL_MUX_DEVICE) {
-    if (out != NULL) {
-      esp = chnGetEventSource((BaseAsynchronousChannel *)serial);
-      /* Register for CHN_TRANSMISSION_END */
-      chEvtRegisterMaskWithFlags(esp, &el, TRACE_SERIAL_EOT_EVT,
-                                         CHN_TRANSMISSION_END);
-      chEvtGetAndClearFlags(&el);
-      /* Write the data to the serial channel. */
-      sdWrite(&SERIAL_MUX_DEVICE, out, len);
+  if (out != NULL) {
+    smd->esp = chnGetEventSource((BaseAsynchronousChannel *)smd->sd);
+    /* Register for CHN_TRANSMISSION_END. To avoid any conflict with other
+       event codes we register the listener with no event. */
 
+    chEvtRegisterMaskWithFlags(smd->esp, &smd->el, 0, CHN_TRANSMISSION_END);
+
+    /* Write the data to the serial channel. */
+    sdWrite(smd->sd, out, len);
+
+    if (timeout != TIME_IMMEDIATE) {
       /* Wait for CHN_TRANSMISSION_END */
-      if (chEvtWaitAnyTimeout(ALL_EVENTS, timeout) == 0) {
+      virtual_timer_t vtp;
+      chVTSet(&vtp, timeout, NULL, NULL);
+      while (chVTIsArmed(&vtp)
+          && (chEvtGetAndClearFlags(&smd->el) | CHN_TRANSMISSION_END) == 0);
+      if (!chVTIsArmed(&vtp)) {
         msg = MSG_TIMEOUT;
       }
-      pktUnregisterEventListener(esp, &el);
+      pktUnregisterEventListener(smd->esp, &smd->el);
     }
-    sdStop(&SERIAL_MUX_DEVICE);
-    chBSemSignal(&serial_mux);
   }
+
+  /* Stop serial driver. */
+  sdStop(smd->sd);
+
+  /* Remove the GPIO map to the UART. */
+  if (mcfg->close.rx.line != PAL_NOLINE) {
+    if (mcfg->close.rx.mode == PAL_MODE_RESET) {
+      palSetLineMode(mcfg->close.rx.line, PAL_MODE_INPUT);
+    } else {
+      palSetLineMode(mcfg->close.rx.line, mcfg->close.rx.mode);
+    }
+  }
+  if (mcfg->close.tx.line != PAL_NOLINE) {
+    if (mcfg->close.tx.mode == PAL_MODE_RESET) {
+      palSetLine(mcfg->close.tx.line);
+      palSetLineMode(mcfg->close.tx.line, PAL_MODE_OUTPUT_PUSHPULL);
+    } else {
+      palSetLineMode(mcfg->close.tx.line, mcfg->close.tx.mode);
+    }
+  }
+  /* Release port lock. */
+  chBSemSignal(&smd->msem);
   return msg;
-#endif
-  (void)serial;
-  (void)out;
-  (void)len;
-  (void)timeout;
-  return MSG_ERROR;
 }
+
+#endif /* HAL_USE_SERIAL */
 
 /** @} */
--- tracker/software/source/drivers/mcu/serialmux.h
+++ tracker/software/source/drivers/mcu/serialmux.h
@@ -4,7 +4,7 @@
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/
+ */
 
 /**
  * @file    serialmux.h
@@ -18,15 +18,47 @@
 #define SERIALMUX_H
 
 /*===========================================================================*/
-/* Module data structures and types.                                         */
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Configuration options
+ * @{
+ */
+/**
+ * @brief   Multiplexed serial driver for USART1 enable switch.
+ * @details If set to @p TRUE support for multiplexed serial using USART1 is included.
+ * @note    The default is @p FALSE.
+ */
+#if !defined(STM32_SERIAL_USE_MUX) || defined(__DOXYGEN__)
+#define STM32_SERIAL_USE_MUX             TRUE
+#endif
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
 /*===========================================================================*/
 
 #include "pkttypes.h"
 
-typedef struct serialMuxGPIO {
-	ioline_cfg_t rx;
-	ioline_cfg_t tx;
-} smux_gpio_t;
+/* GPIO configuration. */
+typedef struct {
+  struct {
+    ioline_cfg_t rx;
+    ioline_cfg_t tx;
+  } open;
+  struct {
+    ioline_cfg_t rx;
+    ioline_cfg_t tx;
+  } close;
+} SerialMuxConfig;
+
+/* Driver structure. */
+typedef struct {
+  binary_semaphore_t    msem;
+  SerialDriver          *sd;
+  event_source_t        *esp;
+  event_listener_t      el;
+} SerialMuxDriver;
 
 /*===========================================================================*/
 /* External declarations.                                                    */
@@ -35,20 +67,15 @@ typedef struct serialMuxGPIO {
 #ifdef __cplusplus
 extern "C" {
 #endif
-  msg_t pktConnectMuxedSerial(const SerialDriver *serial,
-		                      const SerialConfig *cfg,
-							  const smux_gpio_t *gpio,
-                              const sysinterval_t timeout);
-  msg_t pktDisconnectMuxedSerial(const SerialDriver *serial,
-  							     const smux_gpio_t *gpio,
-  							     const sysinterval_t timeout);
-  msg_t pktAcquireMuxedSerial(const SerialDriver *serial,
-		                      const sysinterval_t timeout);
-  void  pktReleaseMuxedSerial(const SerialDriver *serial);
-  msg_t pktReleaseMuxedSerialEOT(const SerialDriver *serial,
-		                         const uint8_t *out,
-                                 const size_t len,
-								 const sysinterval_t timeout);
+  msg_t pktOpenMuxedSerial(SerialMuxDriver *const smd,
+                           const SerialConfig *scfg,
+                           const SerialMuxConfig *mcfg,
+                           const sysinterval_t timeout);
+  msg_t pktCloseMuxedSerial(SerialMuxDriver *const smd,
+                            const SerialMuxConfig *gpio,
+                            const uint8_t *out,
+                            const size_t len,
+                            const sysinterval_t timeout);
 #ifdef __cplusplus
 }
 #endif
